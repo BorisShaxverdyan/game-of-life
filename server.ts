6ec28e6fdb80038646f573395607b3e924f24b65
BorisShaxverdyan <boris.shaxverdyan@tumo.org>
2021-05-26 22:48:19 +0400

    socket connection

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as http from "http";
+import { Server } from "socket.io";
 
 // #region Server stuff
 const port = 3000;
@@ -15,5 +16,18 @@ app.get("/", (req: any, res: any) => {
 
 server.listen(port, function () {
 	console.log(`Application url: http://localhost:${port}/\n`);
+});
+// #endregion
+
+// #region socket connection
+const io = new Server(server);
+
+io.on("connection", socket => {
+	console.log("connected: ", socket.id);
+
+	setInterval(() => {
+		// socket.emit("data", {
+		// });
+	}, 1000);
 });
 // #endregion
